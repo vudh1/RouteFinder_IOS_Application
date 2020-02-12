4b0428d64df1a39e42611a88d28b1f7c8ce39ed8
Daniel Vu <danielvu2810@gmail.com>
2020-02-12 13:32:48 -0800

    First commit

---
+++ RouteFinder-Application/RouteFinder-Application/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RouteFinder-Application
+//
+//  Created by Daniel Vu on 2/12/20.
+//  Copyright © 2020 UC Irvine. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
