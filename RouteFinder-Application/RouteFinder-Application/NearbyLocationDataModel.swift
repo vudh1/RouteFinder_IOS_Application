97a24814993090bfc72625af641fb60cea316a72
Daniel Vu <danielvu2810@gmail.com>
2020-02-13 12:50:09 -0800

    Add Nearby Location Searching Feature

---
+++ RouteFinder-Application/RouteFinder-Application/NearbyLocationDataModel.swift
@@ -0,0 +1,9 @@
+//
+//  NearbyLocationDataModel.swift
+//  RouteFinder-Application
+//
+//  Created by Daniel Vu on 2/13/20.
+//  Copyright © 2020 UC Irvine. All rights reserved.
+//
+
+import Foundation
