7ad2e5b4732b8e2e74929aecd646d1538e1de731
Daniel Vu <danielvu2810@gmail.com>
2020-02-17 12:01:46 -0800

    Add Lauch Map Feature From App

---
+++ RouteFinder-Application/RouteFinder-Application/LocationCell.swift
@@ -0,0 +1,24 @@
+//
+//  LocationCell.swift
+//  RouteFinder-Application
+//
+//  Created by Daniel Vu on 2/16/20.
+//  Copyright © 2020 UC Irvine. All rights reserved.
+//
+
+import UIKit
+
+class LocationCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
