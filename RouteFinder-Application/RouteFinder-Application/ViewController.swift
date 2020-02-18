8a0dc892d3f18191872d1b2c42cbc0b2e6fe5f7e
Daniel Vu <danielvu2810@gmail.com>
2020-02-18 15:16:45 -0800

    Make Launch Map Button Clickable In Whole Cell

--- RouteFinder-Application/RouteFinder-Application/ViewController.swift
+++ RouteFinder-Application/RouteFinder-Application/ViewController.swift
@@ -58,6 +58,7 @@ class ViewController: UIViewController,CLLocationManagerDelegate,UITableViewDele
         locationManager.requestWhenInUseAuthorization()
         locationManager.startUpdatingLocation()
     }
+
     
 
     override func viewDidLoad() {
@@ -254,7 +255,7 @@ class ViewController: UIViewController,CLLocationManagerDelegate,UITableViewDele
 /***************************************************************/
 
     func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
-        return range.location < 6
+        return range.location < 5
     }
     
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
